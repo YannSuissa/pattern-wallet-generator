05255c399cb0cd4829de5f15ae78f035e65842f0
Yann Suissa <yann.suissa@gmail.com>
2023-03-17 23:59:06 +0100

    v1.0.1

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const gen_private = require('./gen_private')
 
-const with_seed = true
-const without_seed = false
-
-function gen_keys() {
-  let wallet;
-
-  wallet = gen_private("0x4242", with_seed)
-  console.log("return value", wallet)
-  wallet = gen_private("0x424242", without_seed)
-  console.log("return value", wallet)
+function gen_keys(pattern, with_seed) {
+  return (gen_private(pattern, with_seed))
 }
 
 module.exports = gen_keys
--- test/script.js
+++ test/script.js
@@ -1,5 +1,13 @@
  
 const wallet_gen = require('pattern-wallet-generator')
 
+const with_seed = true
+const without_seed = false
+
+let wallet;
+
 // console.log(helloNpm())
-wallet_gen()
+wallet = wallet_gen("0x4242", with_seed)
+console.log("return value", wallet)
+wallet = wallet_gen("0x424242", without_seed)
+console.log("return value", wallet)
