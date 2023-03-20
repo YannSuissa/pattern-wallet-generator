54f74a993d472a7fa7d9eba8ea9c8dbca0c677b0
Yann Suissa <yann.suissa@gmail.com>
2023-03-20 18:49:06 +0100

    optimisations

--- index.js
+++ index.js
@@ -6,25 +6,30 @@ let last_sec
 let cpt = 0
 let time_start
 
-let p_pattern, p_with_seed, p_debug;
+let p_pattern, p_with_seed, p_debug, p_odds;
 
 function print(pid, last) {
 
   curr = performance.now();
-  let sec = Number(((curr - time_start) / 1000).toFixed(2))
-  let per_sec = Number((cpt / sec).toFixed(2))
-  if (!last && sec - last_sec < 1)
+  let sec = Number(((curr - time_start) / 1000).toFixed(0))
+  let per_sec = Number((cpt / sec).toFixed(0))
+  if (!last_sec) {
+    last_sec = sec
+    return
+  }
+  if (!last && sec - last_sec < 10)
     return
   last_sec = sec
-  console.log('[' + pid + '] Speed', cpt, '-', per_sec,'/s', "total seconds", sec) 
+  console.log('[' + pid + '] Tries', cpt, '- Speed', per_sec, '/s - ', 
+    "Elapsed", Number((sec / 60).toFixed(0)), "minutes - ",
+    "estimated", Number((p_odds / per_sec / 4800).toFixed(2)), "hours") 
 }
 
 async function find_addr(pid) {
 
-  let id, privateKey, wallet
-  let refresh = 100;
+  let privateKey, wallet
+  let refresh = 500;
 
-  cpt = 0
   time_start = performance.now()
 
   while (1) {
@@ -33,20 +38,20 @@ async function find_addr(pid) {
       wallet = ethers.Wallet.createRandom()
     } 
     else {
-      id = crypto.randomBytes(32).toString('hex');
-      wallet = {}
-      privateKey = "0x"+id;
+      privateKey = "0x" + crypto.randomBytes(32).toString('hex');
       wallet = new ethers.Wallet(privateKey);
-      wallet.publicKey = privateKey
     }
 
-    if (p_debug && !(cpt % refresh)) 
+    if (p_debug && !(--refresh)) {
+      // console.log('refresh', refresh)
       print(pid)
-
-    if (!wallet.address.startsWith(p_pattern)) {
-      continue
+      refresh = 500
     }
+
+    if (!wallet.address.startsWith(p_pattern)) 
+      continue
     else {
+      wallet.publicKey = privateKey
       print(pid, true)
       return wallet
     }
@@ -58,10 +63,11 @@ function gen_private(pattern, with_seed, debug) {
   p_pattern = pattern
   p_with_seed = with_seed
   p_debug = debug
+  p_odds = 16 ** pattern.replace("0x", "").length
 
   if (debug)
     console.log('Starting challenge for', pattern, (with_seed) ? "With seed..." : "Without seed...",
-    "odds are : 1 / ", 16 ** pattern.replace("0x", "").length)
+    "odds are : 1 / ", p_odds)
   
   let obj = {}
   obj = find_addr(0)
